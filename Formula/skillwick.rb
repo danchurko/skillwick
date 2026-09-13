ece454cbda613263e101304115f6fe3bdd30242a
Dan C <55187974+danchurko@users.noreply.github.com>
2026-09-14 00:58:55 +0100

    chore(release): publish v0.2.0 formula

--- Formula/skillwick.rb
+++ Formula/skillwick.rb
@@ -1,14 +1,14 @@
 class Skillwick < Formula
   desc "Find relevant installed local skills"
   homepage "https://github.com/churdaa/skillwick"
-  version "0.1.5"
+  version "0.2.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/churdaa/skillwick/releases/download/v0.1.5/skillwick-aarch64-apple-darwin.tar.xz"
-    sha256 "ecac5d57e653204c296d558faf4e40edbf45aae4256a7fe00b512b4bc3bdff65"
+    url "https://github.com/churdaa/skillwick/releases/download/v0.2.0/skillwick-aarch64-apple-darwin.tar.xz"
+    sha256 "a4a9cfff5b91c215f784d1f5fba4896350a133343f5544f974d6d589dd90172d"
   else
-    url "https://github.com/churdaa/skillwick/releases/download/v0.1.5/skillwick-x86_64-apple-darwin.tar.xz"
-    sha256 "e1d2fe3781ecd779adebee6cb6233a28a0948995b442f4459cfd93bccfcbf098"
+    url "https://github.com/churdaa/skillwick/releases/download/v0.2.0/skillwick-x86_64-apple-darwin.tar.xz"
+    sha256 "a6d9ea3f98f1635495b32f931c6938290005cb88e47816c176557f4e0d778a77"
   end
 
   def install
